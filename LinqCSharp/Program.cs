526085ee33bde532404e3f19b2797fe710575e47
Pain31415 <Cherniy412@ukr.net>
2024-03-26 19:07:08 +0200

    Add project files.

---
+++ LinqCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinqCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
